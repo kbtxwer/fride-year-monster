0ea1b12ee3809f82b2e466a4d35e2fb922a2f11a
kbtxwer <57350104+kbtxwer@users.noreply.github.com>
2022-01-15 00:33:47 +0800

    Update 炸年兽.js
    出了一堆意想不到的bug，暂时回退回去吧

--- 炸年兽.js
+++ 炸年兽.js
@@ -61,32 +61,25 @@ function init() {
             }, 1000);
         }
     });
-	
-	
-	var repeatTimes;
-	while(true){
-		repeatTimes = 6;
-		//重复执行若干次
-		while (repeatTimes-- > 0) {
-			//调试用，找到按钮ID后，修改代码即可注释本句
-			//findHuodongBtnId();
-			transitioPperation();
-			enterActivity();
-			closePopups();
-			openTaskList();
-			recoverApp();
-			var flag = getNeedSelector();
-			viewTask(flag);
-			viewProduct();
-			addMarketCar();
-		}
-		//执行一次收爆竹大约耗时40分钟，相比之下执行常规任务的耗时可以忽略
-		repeatTimes = 27;
-		//执行收爆竹
-		while(repeatTimes-- > 0){
-			regularTask();
-		}
-	}
+ 
+    while (true) {
+ 
+        transitioPperation();
+ 
+        enterActivity();
+ 
+        recoverApp();
+ 
+        var flag = getNeedSelector();
+ 
+        viewTask(flag);
+ 
+        viewProduct();
+         
+        addMarketCar();
+ 
+    }
+ 
  
 }
  
@@ -102,120 +95,6 @@ function start() {
     }
     console.show();
 }
-
-function closePopups(){
-	while (textContains("继续环游").exists() | textContains("立即抽奖").exists() | textContains("开启今日环游").exists() | textContains("点我签到").exists() | textContains("开心收下").exists()) {
-		CustomSleep(1, 3, '请稍候…');
-		if (textContains("继续环游").exists()) {
-			console.log("继续环游");
-			textContains("继续环游").findOne().click();
-			CustomSleep(0.5, 1.5, '请稍候…');
-		} else if (textContains("立即抽奖").exists()) {
-			console.log("关闭立即抽奖");
-			textContains("立即抽奖").findOne().parent().child(1).click();
-			CustomSleep(0.5, 1.5, '请稍候…');
-		} else if (textContains("开启今日环游").exists()) {
-			console.log("开启今日环游");
-			textContains("开启今日环游").findOne().click();
-			CustomSleep(1, 3, '请稍候…');
-		} else if (textContains("点我签到").exists()) {
-			console.log("点我签到");
-			textContains("点我签到").findOne().parent().click();
-			CustomSleep(1, 3, '请稍候…');
-			textContains("开心收下").waitFor();
-			textContains("开心收下").findOne().parent().click();
-			CustomSleep(1, 3, '请稍候…');
-		} else if (text("开心收下开心收下").exists()) {
-			console.log("开心收下");
-			text("开心收下开心收下").findOne().click();
-			CustomSleep(1, 3, '请稍候…');
-		} else if (textContains("开心收下").exists()) {
-			console.log("开心收下");
-			textContains("开心收下").findOne().parent().click();
-			CustomSleep(1, 3, '请稍候…');
-		} else {
-			console.log("暂无可处理弹窗");
-			break;
-		}
-		CustomSleep(1, 3, '请稍候…');
-	}
-	console.log("如还有弹窗，请手动处理");
-	CustomSleep(3, 5, '请稍候…');
-
-	if (text("立即前往").exists()) {
-		console.log("前往签到");
-		textContains("立即前往").findOne().parent().click();
-		CustomSleep(0.5, 1.5, '请稍候…');
-		console.log("点我签到");
-		textContains("点我签到").findOne().parent().click();
-		CustomSleep(1, 3, '请稍候…');
-		textContains("开心收下").waitFor();
-		textContains("开心收下").findOne().parent().click();
-		CustomSleep(1, 3, '请稍候…');
-	}
-	
-	if (text("放入我的＞我的优惠券").exists()) {
-		text("放入我的＞我的优惠券").findOne().parent().parent().child(0).click();
-		CustomSleep(1, 3, '请稍候…');
-	}
-}
-
-function regularTask(noCollect){
-	
-	CustomSleep(2, 4, '请稍候…');
-	if (!textMatches(/.*消耗.*爆竹/).exists()){
-		console.log("当前不在活动界面！");
-		var i;
-		for(i=0;i<10;i++){
-			back();
-			if(textMatches(/.*消耗.*爆竹/).exists()){
-				console.log("成功回到活动界面！");
-			}
-			sleep(1000);
-		}
-		if(i==10){
-			launch(appName);
-			enterActivity();
-		}
-	}
-	console.log("成功进入活动界面");
-	console.log("等待加载弹窗……");
-
-	closePopups();
-	
-	if(noCollect==true) return;
-	
-	if (text("爆竹满了~~").exists()) {
-		console.log("爆竹已存满");
-		text("爆竹满了~~").findOne().parent().parent().child(2).click();
-		console.log("爆竹领取成功");
-		CustomSleep(2, 4, '请稍候…');
-	} else if (textContains("后满").exists()) {
-		CustomSleep(30*60, 45*60, '随机等待 30-45 min 后收集爆竹');
-		//由于等待时间过长，需要再检查一下有没有蹦弹窗出来
-		regularTask(true)
-		textContains("后满").findOne().parent().parent().child(2).click();
-		console.log("爆竹领取成功");
-	}
-}
-
-function findHuodongBtnId(){
-	var btn_id = 0;
-	var max_btn_id = 30;
-	while(btn_id++ < max_btn_id){
-		var button = className('android.view.View')
-					.depth(14)
-					.indexInParent(btn_id)
-					.drawingOrder(0)
-					.clickable();
-		if (button.exists()) {
-			console.info("当前遍历按钮id",btn_id)
-			var rect = button.findOne().bounds();
-			console.info("点击",rect.centerX(),rect.centerY(),"附近")
-			sleep(1000);
-		}
-	}
-}
  
 /**
  * 进入做任务界面
@@ -225,44 +104,39 @@ function enterActivity() {
         sleep(4000);
         if (text("累计任务奖励").exists()) {
             console.info("已经在任务界面");
-			console.info("活动参数：" + huodong_indexInParent_num)
             sleep(1000);
             headerXY = id("a96").findOne().bounds();
         } else {
             if (desc("浮层活动").exists()) {
                 console.info("点击浮层活动");
                 var huodong = desc("浮层活动").findOne().bounds();
-				//这个坐标是对的
-				console.info("点击",huodong.centerX(),huodong.centerY(),"附近")
-                //快速双击，当浮层半透明的时候必须双击才能进入
+                randomClick(huodong.centerX(), huodong.centerY());
+				sleep(150);
 				randomClick(huodong.centerX(), huodong.centerY());
-				sleep(150)
-				randomClick(huodong.centerX(), huodong.centerY());
-                sleep(5000);
+                sleep(1000);
+ 
+            }
+ 
+            // 获取进入做任务界面的控件
+            var button = className('android.view.View')
+                .depth(14)
+                .indexInParent(huodong_indexInParent_num)
+                .drawingOrder(0)
+                .clickable();
+            if (button.exists()) {
+                console.info("点击进入做任务界面")
+                var rect = button.findOne().bounds();
+                randomClick(rect.centerX(), rect.centerY());
+                sleep(1000);
+                headerXY = id("a96").findOne().bounds();
+ 
+            } else {
+                console.log('当前可能不在活动页面')
+				back()
             }
         }
         sleep(1000);
     }
-}
-
-function openTaskList(){
-	// 获取进入做任务界面的控件
-	var button = className('android.view.View')
-		.depth(14)
-		.indexInParent(huodong_indexInParent_num)
-		.drawingOrder(0)
-		.clickable();
-	if (button.exists()) {
-		console.info("点击进入做任务界面",huodong_indexInParent_num)
-		var rect = button.findOne().bounds();
-		console.info("点击",rect.centerX(),rect.centerY(),"附近")
-		randomClick(rect.centerX(), rect.centerY());
-		sleep(1000);
-		headerXY = id("a96").findOne().bounds();
-	} else {
-		huodong_indexInParent_num = huodong_indexInParent_num==17?18:17;
-		console.info("如果没有进入做任务界面，请直接回到活动首页，脚本尝试变更按钮ID为",huodong_indexInParent_num);
-	}
 }
  
 /**
@@ -349,7 +223,6 @@ function viewTask(flag) {
         } else if (text("消息").exists() && text("扫啊扫").exists()) {
             console.warn("因为某些原因回到首页，重新进入活动界面");
             enterActivity();
-			openTaskList();
         } else if (text("天天都能领").exists()) {
             sleep(2000);
             console.info("天天都能领");
@@ -534,7 +407,7 @@ function getNeedSelector() {
                 }
  
                 // 如果是浏览就返回的任务，将标记设为true
-                isBackFlag = (TASK_LIST[i].indexOf("浏览可得") >= 0 || TASK_LIST[i].indexOf("浏览并关注") >= 0) ? true : false;
+                isBackFlag = (TASK_LIST[i].indexOf("浏览可得") >= 0 || TASK_LIST[i].indexOf("浏览并关注可得2000") >= 0) ? true : false;
                 // 如果是小程序任务，将小程序标记设为true
                 isXcx = (TASK_LIST[i].indexOf("小程序") >= 0) ? true : false;
                 var rect = allSelector[current_task_num].bounds();
@@ -560,11 +433,11 @@ function viewAndFollow() {
  
 /**
  * 自动判断程序是否卡顿，恢复方法
- * 判断依据：1.不在活动首页 2.停留时间超过30s
+ * 判断依据：1.不在活动界面 2.停留某个界面长达30s
  * @returns 
  */
 function recoverApp() {
-    if ( ( !textMatches(/.*消耗.*爆竹/).exists() ) && JUDGE_TIME > 30) {
+    if (!text("累计任务奖励").exists() && JUDGE_TIME > 30) {
         if (back()) {
             // 计时器重置
             JUDGE_TIME = 0;
@@ -608,47 +481,12 @@ function transitioPperation() {
  * @param {纵坐标} y 
  */
 function randomClick(x, y) {
-    var rx = random(-10, 10);
-    var ry = random(-10, 10);
-	console.info("随机点击(", x + rx,',',y + ry,")处")
+    var rx = random(0, 5);
+    var ry = random(0, 5);
+	console.log("点击了(", x+rx ,",", y+ry ,")")
     click(x + rx, y + ry);
     sleep(2000);
     return true;
-}
-
-//自定义延迟
-//minNum~maxNum：延迟范围
-//msg：提示消息
-//scroll：是否自动滚屏
-function CustomSleep(minNum, maxNum, msg, scroll) {
-    console.info(msg);
-    if (maxNum > 0 && maxNum >= minNum) {
-        var sleeptimes = parseInt(random(minNum * 1000, maxNum * 1000), 10);
-        var tick = 1000;
-        if (sleeptimes > tick) {
-            for (var i = 0; i < sleeptimes; i += tick) {				
-				console.verbose("……【" + (100 - parseInt((sleeptimes - i) * 100 / (sleeptimes), 10)) + "%】……");
-                var sleeptick = Math.min(tick, sleeptimes - i);
-                sleep(sleeptick);
-                if (scroll && i % 3 == 1) scrollDown();
-            }
-        }
-        else {
-            sleep(sleeptimes);
-            if (scroll) scrollDown();
-        }
-        console.verbose("……【100%】……");
-    }
-}
-//自定义返回
-function CustomBack() {
-    back();
-    CustomSleep(1, 3, "返回中");
-}
-//自定义结束任务
-function CustomExit() {
-    CustomSleep(1, 3, "脚本即将终止运行");
-    exit();
 }
  
 /**
